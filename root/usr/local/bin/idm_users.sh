27e8e25f689c2df8983e6e0794716b8930b10d60
Jim Wildman <jwildman@redhat.com>
2016-06-05 14:54:00 -0400

    adding uid

--- root/usr/local/bin/idm_users.sh
+++ root/usr/local/bin/idm_users.sh
@@ -2,6 +2,6 @@ for i in 1 2 3 4
 do
  echo "------"
  echo classuser$i
- ipa user-add classuser$i --password --first=User$i --last=Class
+ ipa user-add classuser$i --password --first=User$i --last=Class --uid=1000$i
  echo ""
 done
